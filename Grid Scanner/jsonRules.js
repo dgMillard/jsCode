2a995f0a11e8b50fbc702aed025ed7fbc5eb86f3
dgMillard <duncanMillard@live.com>
2013-05-23 15:33:49 -0700

    Update jsonRules.js

--- Grid Scanner/jsonRules.js
+++ Grid Scanner/jsonRules.js
@@ -1,32 +1,48 @@
 componentValidator.run(
 {
-    "ruleSet": [
-        {
-            "title": "Rule for testClass1",
-            "className": "testClass1",
-            "criteria": [
-                "h1",
-                "p"
-            ]
-        },
-        {
-            "title": "Rule for gd13-pilot",
-            "className": "gd13-pilot",
-            "criteria": [
-                ".frame",
-                ".c50-text",
-                "#thumbnails"
-            ]
-        },
-        {
-            "title": "Rule for gd14-pilot",
-            "className": "gd14-pilot",
-            "criteria": [
-                ".frame",
-                ".c50-text",
-                "#thumbnails"
-            ]
-        }
-    ]
+   "ruleSet":[
+      {
+         "behavior" : "loose",
+         "title":"Rule for .c50-pilot",
+         "target":".c50-pilot",
+         "criteriaList":[
+               ".frame",
+               ".c50-text",
+               "#thumbnails",
+               function(target){
+                  target = jQuery(target);
+                  return true;
+               }
+         ]
+      },
+      {
+         "behavior" : "loose",
+         "title":"Rule for .c47-pilot",
+         "target":".c47-pilot",
+         "criteriaList":[
+              "h3",
+               "p", 
+              ".cta-links"
+         ]
+      },
+      {
+         "behavior" : "loose",
+         "title":"Rule for .c46-pilot",
+         "target":".c46-pilot",
+         "criteriaList":[
+               "h2"
+         ]
+      },
+      {
+         "behavior" : "strict",
+         "title":"Test rule",
+         "target":".test",
+         "criteriaList":[
+               "h1",
+               "h1",
+               "p"
+         ]
+      }
+   ]
 }
 );
