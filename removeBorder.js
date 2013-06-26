8b55d44a2f8d4cf4f806f75146662b4e0c84f133
dgMillard <duncanMillard@live.com>
2013-06-26 12:00:37 -0700

    Update removeBorder.js

--- removeBorder.js
+++ removeBorder.js
@@ -1,5 +1,7 @@
 (function(){
 	var cssElement = document.getElementById("CQ");
-	cssElement.style.cqEditrolloverInsertContainerDisplay = 'none';
+	//cssElement.style.cqEditrolloverInsertContainerDisplay = 'none';
+	cssElement.style.display = 'none';
+
 	//alert(csselement.style(['cq-editrollover-insert-container-display']));
 })();
