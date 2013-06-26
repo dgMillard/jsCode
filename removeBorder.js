70afa663a290d73d5d3e57206efa5bf241647631
dgMillard <duncanMillard@live.com>
2013-06-26 14:39:16 -0700

    Update removeBorder.js

--- removeBorder.js
+++ removeBorder.js
@@ -1,39 +1,8 @@
 (function(){
-	//var cssElement = document.getElementById("CQ");
-	//alert(cssElement.id);
-	//cssElement.style.cqEditrolloverInsertContainerDisplay = 'none';
-	//cssElement.style.display = 'none';
-	//alert(cssElement.style.cqEditrolloverHighlightBackgroundColor);
-	//alert(csselement.style(['cq-editrollover-insert-container-display']));
-	var classes = new Array();
-	var useCssRules = true;
-	//if(document.styleSheets[i].rules)
-		//useCssRules = false;
-	
-	for(var i=0; i < document.styleSheets.length; i++)
-	{
-		if(useCssRules)
-		{
-			for(var j=0; j < document.styleSheets[i].cssRules.length; j++)
-			{
-				classes.push(document.styleSheets[i].cssRules[j]);
-			}
-		}
-		else
-		{
-			for(var j=0; j < document.styleSheets[i].rules.length; j++)
-			{
-				classes.push(document.styleSheets[i].rules[j]);
-			}
-		}
-	}
-	var results = new Array();
-  	for(var x=0; x < classes.length; x++) {
-  		if(classes[x].cssText.lastIndexOf("#CQ .cq-editrollover-highlight", 0) === 0)
-  		{
-  			results.push(classes[x]);
-  			alert(classes[x].cssText);	
-  		}
-  	}	
 
+
+	var headTag = document.getElementsByTagName("head")[0].innerHTML;	
+	var newCSS = headTag + '<style type="text/css">#CQ .cq-editrollover-insert-container {display: none;}</style>';
+	document.getElementsByTagName('head')[0].innerHTML = newCSS;
+ 
 })();
