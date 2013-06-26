0ed073dd8742e7e37cdf986a3770ddd743d6f507
dgMillard <duncanMillard@live.com>
2013-06-26 13:54:50 -0700

    Update removeBorder.js

--- removeBorder.js
+++ removeBorder.js
@@ -1,8 +1,13 @@
 (function(){
-	var cssElement = document.getElementById("CQ");
-	alert(cssElement.id);
+	//var cssElement = document.getElementById("CQ");
+	//alert(cssElement.id);
 	//cssElement.style.cqEditrolloverInsertContainerDisplay = 'none';
 	//cssElement.style.display = 'none';
-	alert(cssElement.style.cqEditrolloverHighlightBackgroundColor);
+	//alert(cssElement.style.cqEditrolloverHighlightBackgroundColor);
 	//alert(csselement.style(['cq-editrollover-insert-container-display']));
+	
+	var classes = document.styleSheets[0].rules || document.styleSheets[0].cssRules;
+  	for(var x=0;x<classes.length;x++) {
+  		alert(classes[x].cssText);
+  	}
 })();
