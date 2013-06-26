9378052cc2cbf1e271960de2c716e038f0f7ac86
dgMillard <duncanMillard@live.com>
2013-06-26 14:43:17 -0700

    Update removeBorder.js

--- removeBorder.js
+++ removeBorder.js
@@ -2,7 +2,7 @@
 
 
 	var headTag = document.getElementsByTagName("head")[0].innerHTML;	
-	var newCSS = headTag + '<style type="text/css">#CQ .cq-editrollover-highlight-right {display: none;}</style>';
+	var newCSS = headTag + '<style type="text/css">#CQ .cq-editrollover-highlight-left {display: none;}</style>';
 	document.getElementsByTagName('head')[0].innerHTML = newCSS;
  
 })();
