f90348f2515cc09aa319ca637c00cb838db97761
dgMillard <duncanMillard@live.com>
2013-06-27 10:40:14 -0700

    Update removeBorder.js

--- removeBorder.js
+++ removeBorder.js
@@ -1,8 +1,8 @@
 (function(){
 
-
-
-	var newCSS = '<style type="text/css">#CQ .cq-editrollover-highlight-left {backgroundColor: #7d6d5a;}</style>';
-	document.getElementsByTagName('head')[0].appendChild(newCSS);
+	var div = document.getElementById("CQ"); // myDiv is the id right?
+	var css = div.getAttribute("style");
+	var style = 'background-color: #ffff00;';
+	css += style;
  
 })();
