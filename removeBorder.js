6f03ec512f7ea468197145ed1ee42f689b718d36
dgMillard <duncanMillard@live.com>
2013-06-28 11:47:43 -0700

    Update removeBorder.js

--- removeBorder.js
+++ removeBorder.js
@@ -1,38 +1,28 @@
 (function(){
-//	for(var i=0; i < document.styleSheets.length; i++)
-//	{
-//		var currentSheet=document.styleSheets[i];
-//		var pageRules=currentSheet.cssRules? currentSheet.cssRules: currentSheet.rules //Makes this browser independent
-//		for (var j=0; j<pageRules.length; j++){
-//			if(pageRules[j].selectorText.toLowerCase()=='#cq .cq-editrollover-highlight-left')
-//			{//Rule located
-//				alert("Target found");
-//				pageRules[j].style.zIndex = '0 !important';
-//				alert(pageRules[j].style.zIndex);
-//				alert(pageRules[j].cssText);
-//			}
-//		}
-//	}
-var v = "1.3.2";
-// check prior inclusion and version
-if (window.jQuery === undefined || window.jQuery.fn.jquery < v) { // If the current page either doesn't have jQuery or is out of date
-var done = false;
-var script = document.createElement("script");
-script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
-script.onload = script.onreadystatechange = function(){
-if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
-  done = true;
-  loadedJ();
-  }
-};
-document.getElementsByTagName("head")[0].appendChild(script);
-}
-else{
-  loadedJ();
-}
 
-function loadedJ()
-{
-  $('.cq-editrollover-highlight-left').css('display', 'none !important');
-}
+  // the minimum version of jQuery we want
+	var v = "1.3.2";
+
+	// check prior inclusion and version
+	if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
+		var done = false;
+		var script = document.createElement("script");
+		script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
+		script.onload = script.onreadystatechange = function(){
+			if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+				done = true;
+				initMyBookmarklet();
+			}
+		};
+		document.getElementsByTagName("head")[0].appendChild(script);
+	} else {
+		initMyBookmarklet();
+	}
+	
+	function initMyBookmarklet() {
+		(window.myBookmarklet = function() {
+			$('.cq-editrollover-highlight-left').css('display', 'none !important');
+		})();
+	}
+
 })();
