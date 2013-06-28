eca9f3b715f038dcf3997fc94e089c72ddb10ee8
dgMillard <duncanMillard@live.com>
2013-06-28 13:50:24 -0700

    Update removeBorder.js

--- removeBorder.js
+++ removeBorder.js
@@ -23,7 +23,7 @@
 		(window.myBookmarklet = function() {
 			//$('.cq-editrollover-highlight-left').css('display', 'none !important');
 			//jQuery('.cq-editrollover-highlight-left').css({'display':'none !important'});
-			jQuery('.cq-editrollover-highlight-left').each(function() 
+			jQuery('.cqEditrolloverHighlightLeft').each(function() 
 			{
 				alert("Target Found");
 				$(this).css('display', 'none !important');
