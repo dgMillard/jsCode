6f916769aa9d88d293025b3cda4100db16afc910
dgMillard <duncanMillard@live.com>
2013-06-28 11:07:12 -0700

    Update removeBorder.js

--- removeBorder.js
+++ removeBorder.js
@@ -1,13 +1,27 @@
 (function(){
-	var newCss = '#CQ .cq-editrollover-highlight-left { display: none !important;}';
-	var appendee = document.createElement('style');
-	appendee.type = 'text/css';
-	if (appendee.styleSheet) {//Internet Exploder
-		appendee.styleSheet.cssText = newCss;
-	} else {//Alternate Browsers		
-		appendee.innerHTML = newCss;
-	}
+//	var newCss = '#CQ .cq-editrollover-highlight-left { display: none !important;}';
+//	var appendee = document.createElement('style');
+//	appendee.type = 'text/css';
+//	if (appendee.styleSheet) {//Internet Exploder
+//		appendee.styleSheet.cssText = newCss;
+//	} else {//Alternate Browsers		
+//		appendee.innerHTML = newCss;
+//	}
+//
+//	document.getElementsByTagName("head")[0].appendChild(appendee);
+//	console.log(appendee.innerHTML);
 
-	document.getElementsByTagName("head")[0].appendChild(appendee);
-	console.log(appendee.innerHTML);
+	for(var i=0; i < document.styleSheets.length; i++)
+	{
+		var currentSheet=document.styleSheets[i];
+		var pageRules=currentSheet.cssRules? currentSheet.cssRules: currentSheet.rules //Makes this browser independent
+		for (var j=0; j<pageRules.length; j++){
+			if(pageRules[j].selectorText.toLowerCase()=="#CQ .cq-editrollover-highlight-left")
+			{//Rule located
+				pageRules[j].style.display = "none";
+				break;
+			}
+		}
+	}
+}
 })();
