377ef4d3d7a86ca3fcfd49938ea4f49be4a42f6c
dgMillard <duncanMillard@live.com>
2013-06-28 10:18:14 -0700

    Update removeBorder.js

--- removeBorder.js
+++ removeBorder.js
@@ -1,8 +1,13 @@
 (function(){
+	var newCss = '#CQ .cq-editrollover-highlight-left { z-index: 0 !important;'};
+	var appendee = document.createElement('style');
+	appendee.type = 'text/css';
+	if (appendee.styleSheet) {//Internet Exploder
+		appendee.styleSheet.cssText = newCss;
+	} else {//Alternate Browsers		
+		appendee.innerHTML = newCss;
+	}
 
-	var div = document.getElementById("CQ"); // myDiv is the id right?
-	var css = div.getAttribute("style");
-	var style = 'background-color: #ffff00;';
-	css += style;
+	document.getElementsByTagName("head")[0].appendChild(appendee);
  
 })();
