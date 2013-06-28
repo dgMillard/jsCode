de75cbcfcc57845126233bc7a967656a961d20cc
dgMillard <duncanMillard@live.com>
2013-06-28 11:40:46 -0700

    Update removeBorder.js

--- removeBorder.js
+++ removeBorder.js
@@ -13,7 +13,23 @@
 //			}
 //		}
 //	}
-
-
+var v = "1.3.2";
+// check prior inclusion and version
+if (window.jQuery === undefined || window.jQuery.fn.jquery < v) { // If the current page either doesn't have jQuery or is out of date
+var done = false;
+var script = document.createElement("script");
+script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
+script.onload = script.onreadystatechange = function(){
+if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+done = true;
 $('.cq-editrollover-highlight-left').css('display', 'none !important');
+}
+};
+document.getElementsByTagName("head")[0].appendChild(script);
+}
+else{
+$('.cq-editrollover-highlight-left').css('display', 'none !important');
+}
+
+
 })();
