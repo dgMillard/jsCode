5a20342f4e19621bca248bceeeb67b2bcba0929d
dgMillard <duncanMillard@live.com>
2013-07-15 11:20:56 -0700

    Update removeBorder.js

--- removeBorder.js
+++ removeBorder.js
@@ -2,31 +2,31 @@
 
 var theClass = '.cq-editrollover-highlight-left';
 
-var element = 'display';
-var value = 'none !important';
-var cssRules;
+var element1 = 'display';
+var value1 = 'none !important';
 
-
-for (var S = 0; S < document.styleSheets.length; S++){
-	if (document.styleSheets[S]['rules']) {
-		cssRules = 'rules';
-	} else if (document.styleSheets[S]['cssRules']) {
-		cssRules = 'cssRules';
-	} else {
-	//no rules found... browser unknown
-	}
-	for (var R = 0; R < document.styleSheets[S][cssRules].length; R++) {
-		if (document.styleSheets[S][cssRules][R].selectorText == theClass) {
-			alert("Selector Found! Sheet: " + S + "    Rule: " + R + "    Selector: "+ document.styleSheets[S][cssRules][R].selectorText);
-			if(document.styleSheets[S][cssRules][R].style[element]){
-				document.styleSheets[S][cssRules][R].style[element] = value;
-				alert("Did it!");
-				break;
+ruleChanger(theClass, element1, value1);
+function ruleChanger(selector, rule, value) {
+	for (var S = 0; S < document.styleSheets.length; S++){
+		if (document.styleSheets[S]['rules']) {
+			cssRules = 'rules';
+		} else if (document.styleSheets[S]['cssRules']) {
+			cssRules = 'cssRules';
+		} else {
+		//no rules found... browser unknown
+		}
+		for (var R = 0; R < document.styleSheets[S][cssRules].length; R++) {
+			if (document.styleSheets[S][cssRules][R].selectorText == selector) {
+				alert("Selector Found! Sheet: " + S + "    Rule: " + R + "    Selector: "+ document.styleSheets[S][cssRules][R].selectorText);
+				if(document.styleSheets[S][cssRules][R].style[rule]){
+					document.styleSheets[S][cssRules][R].style[rule] = value;
+					alert("Did it!");
+					break;
+				}
 			}
 		}
 	}
-}
-
+};
 	
 
 alert("script run...");
