bf84582e3419efe1ecbab21b1b2733639879393c
dgMillard <duncanMillard@live.com>
2013-07-15 11:12:00 -0700

    Update removeBorder.js

--- removeBorder.js
+++ removeBorder.js
@@ -9,36 +9,27 @@ var cssRules;
 
 
 for (var S = 0; S < document.styleSheets.length; S++){
-	try{
-		document.styleSheets[S].insertRule(theClass+' { '+element+': '+value+'; }',document.styleSheets[S][cssRules].length);
-	} catch(err){
-		try{document.styleSheets[S].addRule(theClass,element+': '+value+';');
-	}catch(err)
-		{
-			alert("could not insert or add rule to page: " + S);
-			try{
-				if (document.styleSheets[S]['rules']) {
-					cssRules = 'rules';
-				} else if (document.styleSheets[S]['cssRules']) {
-					cssRules = 'cssRules';
-				} else {
-				//no rules found... browser unknown
-				}
-				alert(cssRules);
-				for (var R = 0; R < document.styleSheets[S][cssRules].length; R++) {
-					if (document.styleSheets[S][cssRules][R].selectorText == theClass) {
-						alert("Selector Found!");
-						if(document.styleSheets[S][cssRules][R].style[element]){
-							document.styleSheets[S][cssRules][R].style[element] = value;
-							alert("Did it!");
-							break;
-						}
-					}
-				}
+	if (document.styleSheets[S]['rules']) {
+		cssRules = 'rules';
+	} else if (document.styleSheets[S]['cssRules']) {
+		cssRules = 'cssRules';
+	} else {
+	//no rules found... browser unknown
+	}
+	alert(cssRules);
+	for (var R = 0; R < document.styleSheets[S][cssRules].length; R++) {
+		if (document.styleSheets[S][cssRules][R].selectorText == theClass) {
+			alert("Selector Found!");
+			if(document.styleSheets[S][cssRules][R].style[element]){
+				document.styleSheets[S][cssRules][R].style[element] = value;
+				alert("Did it!");
+				break;
 			}
-			catch (err){}
 		}
 	}
 }
+
+	
+
 alert("script run...");
 })();
