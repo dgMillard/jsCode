f784b1952d5b07a711fe02e100e2b9246340c949
dgMillard <duncanMillard@live.com>
2013-07-15 11:49:57 -0700

    Update removeBorder.js

--- removeBorder.js
+++ removeBorder.js
@@ -1,7 +1,7 @@
 (function(){
 var style = document.createElement('style');
 style.type = 'text/css';
-style.innerHTML = '.elementA { color: red; }';
+style.innerHTML = '.exampleA { color: red; }';
 document.getElementsByTagName('head')[0].appendChild(style);
 
 })();
