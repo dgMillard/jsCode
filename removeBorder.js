aeb923ab2b22a2de77f43ab88d21cf7a07c81bb6
dgMillard <duncanMillard@live.com>
2013-07-15 16:25:12 -0700

    Update removeBorder.js

--- removeBorder.js
+++ removeBorder.js
@@ -1,8 +1,5 @@
 (function(){
-var style = document.createElement('style');
-style.type = 'text/css';
-style.innerHTML = '#w1x #CQ .cq-editrollover-highlight-left { background-color: #003b6f !important; }';
 
-document.getElementsByTagName('head')[0].appendChild(style);
+jQuery('.cqEditrolloverHighlightLeft').remove();
 
 })();
