c95d3b948c22f505a88857830943201c6af5d167
dgMillard <duncanMillard@live.com>
2013-07-15 11:05:15 -0700

    Update removeBorder.js

--- removeBorder.js
+++ removeBorder.js
@@ -25,6 +25,7 @@ for (var S = 0; S < document.styleSheets.length; S++){
 				}
 				for (var R = 0; R < document.styleSheets[S][cssRules].length; R++) {
 					if (document.styleSheets[S][cssRules][R].selectorText == theClass) {
+						alert("Selector Found!");
 						if(document.styleSheets[S][cssRules][R].style[element]){
 							document.styleSheets[S][cssRules][R].style[element] = value;
 							alert("Did it!");
