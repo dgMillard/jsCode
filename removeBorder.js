0db7558f6fda2776ec0d3a94b777dce2f5a5e426
dgMillard <duncanMillard@live.com>
2013-07-16 10:41:06 -0700

    Update removeBorder.js

--- removeBorder.js
+++ removeBorder.js
@@ -1,9 +1,30 @@
 (function(){
 
+$('body').prepend('<p>CQ CleanOTron 1500</p><table> <tr> <td><button onclick="window.componentClear(1)">Run</button> </td> <td>Clear Left Borders</td> </tr> <tr> <td><button onclick="window.componentClear(2)">Run</button> </td> <td>Clear Right Borders</td> </tr> <tr> <td><button onclick="window.componentClear(3)">Run</button> </td> <td>Clear Top Borders</td> </tr> <tr> <td><button onclick="window.componentClear(4)">Run</button> </td> <td>Clear Bottom Borders</td> </tr></table>');
 
 
-jQuery('.cq-editrollover-highlight-left').remove();
-jQuery('.cq-editrollover-highlight-right').remove();
-jQuery('.cq-editrollover-highlight-top').remove();
-jQuery('.cq-editrollover-highlight-bottom').remove();
+window.componentClear = var functionOne = function(section) {
+  switch(n)
+  {
+  case 1:
+    jQuery('.cq-editrollover-highlight-left').remove();
+    break;
+  case 2:
+    jQuery('.cq-editrollover-highlight-right').remove();
+    break;
+  case 3:
+    jQuery('.cq-editrollover-highlight-top').remove();
+    break;
+  case 4:
+    jQuery('.cq-editrollover-highlight-bottom').remove();
+    break;
+  }
+};
+
+
+
+
+
+
+
 })();
