c98c85433238fd07982d02373a8859195f16f614
dgMillard <duncanMillard@live.com>
2013-07-16 11:11:41 -0700

    Update removeBorder.js

--- removeBorder.js
+++ removeBorder.js
@@ -1,26 +1,31 @@
 (function(){
 
-var formHTML = '<p>CQ CleanOTron 1500</p><table> <tr> <td><button onclick="window.componentClear(0)">Run</button> </td> <td>Clear Mouseover Borders</td> </tr> <tr> <td><button onclick="window.componentClear(1)">Run</button> </td> <td>Clear Drop Targets</td> </tr></table>';
-jQuery('body').append(formHTML);
-
-
-window.componentClear = function(section) {
-  switch(section)
-  {
-  case 0:
-    jQuery('.cq-editrollover-highlight-left').remove();
-    jQuery('.cq-editrollover-highlight-right').remove();
-    jQuery('.cq-editrollover-highlight-top').remove();
-    jQuery('.cq-editrollover-highlight-bottom').remove();
-    break;
-  case 1:
-    jQuery('.cq-editrollover-insert-container').remove();
-    break;
-
-  }
-};
-
-
+if(document.URL.search("cf#") != -1)
+{
+  var formHTML = '<p>CQ CleanOTron 1500</p><table> <tr> <td><button onclick="window.componentClear(0)">Run</button> </td> <td>Clear Mouseover Borders</td> </tr> <tr> <td><button onclick="window.componentClear(1)">Run</button> </td> <td>Clear Drop Targets</td> </tr></table>';
+  jQuery('body').append(formHTML);
+  
+  
+  window.componentClear = function(section) {
+    switch(section)
+    {
+    case 0:
+      jQuery('.cq-editrollover-highlight-left').remove();
+      jQuery('.cq-editrollover-highlight-right').remove();
+      jQuery('.cq-editrollover-highlight-top').remove();
+      jQuery('.cq-editrollover-highlight-bottom').remove();
+      break;
+    case 1:
+      jQuery('.cq-editrollover-insert-container').remove();
+      break;
+  
+    }
+  };
+}
+else
+{
+  window.open("www.youtube.com","_self");
+}
 
 
 
